6467bcee8eb99d01f6fc62467acce064e676c6c8
hectorcarrasco1992 <hector.carrasco@codeimmersives.com>
2019-12-05 15:53:35 -0500

    finished

--- main.js
+++ main.js
@@ -1,7 +1,52 @@
 const Ray = () => {
+  return {
+    length:0,
+
+    push: function(value){
+      this[this.length] = value
+      this.length++
+
+      
+    },
+
+    pop: function(){
+      const toBeRemoved = this[this.length-1]
+      delete this[this.length-1]
+      this.length--
+      
+      return toBeRemoved
+    },
+    
+    unshift :function(value){
+      for (let i = this.length;i > 0; i--){
+        this[i]=this[i-1]
+
+      }
+      
+      this.length++
+      this[0]= value
+
+    },
+
+    shift : function(value){
+      const toBeRemoved = this[0]
+      delete this[0]
+      this.length--;
+      for (let i=0; i<this.length+1; i++) {
+        this[i] = this[i+1]
+      }
+      return toBeRemoved
+
+    },
+
+
+    
+
+    
+
   
 }
-
+}
 module.exports = {
   Ray,
 }
